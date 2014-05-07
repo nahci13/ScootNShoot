099740665b9a20902cabf8b98412ee1946461c8d
bencon <bencon@udel.edu>
2014-05-06 21:17:04 -0400

    Now can sort networks based on average scanned signal

--- unconnected.py
+++ unconnected.py
@@ -5,6 +5,9 @@ import string
 class scanData():
 	def __init__(self):
 		self.data = dict()
+		self.averages = dict()
+		self.scanNumber = 0 #keeps track of how many scans have been logged
+		self.sortedAccessPoints = []
 			
 	def scan(self):
 		r = envoy.run("sudo iwlist eth1 scan | egrep 'Quality|ESSID|Address'")
@@ -41,13 +44,48 @@ class scanData():
 					self.data[mac].append([ESSID])
 					self.data[mac].append([signal])
 					#print "here"+str(self.data[mac])
+		self.scanNumber += 1
 
 	def printData(self):
 		#print self.data
 		for x in self.data:
 			print x+str(self.data[x])
 
+	def printAverages(self):
+		for x in self.averages:
+			print x+str(self.averages[x])
+
+
+	def averagedData(self):
+		total = 0
+		for x in self.data:
+			#print self.data[x]
+			print self.data[x][1]
+			for y in self.data[x][1]:
+				total += y
+			total += (100*(self.scanNumber-len(self.data[x][1]))) #add in bad data for every scan that a signal was not found so that the average isn't artificially inflated
+			self.averages[x] = [self.data[x][0], total/self.scanNumber]
+			total = 0
+
+	def sortConnections(self):
+		APs = []
+		count = 0
+		for x in self.data:
+			APs+=[[x,self.averages[x][0][0],self.averages[x][1]]]
+			count += 1
+		self.sortedAccessPoints = sorted(APs,key=getKey)
+		#for x in self.sortedAccessPoints:
+		#	print x
+
+
+def getKey(item):  #needed for the sorting function 
+	return item[2]
+
 a = scanData()
-for x in range(5):
+for x in range(10):
 	a.scan()
 a.printData()
+print "number of scans performed "+str(a.scanNumber)
+a.averagedData()
+a.printAverages()
+a.sortConnections()
