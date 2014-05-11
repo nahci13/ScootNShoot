8778f6afe4fb8ef44dd82573b35ae3fd57d3ca83
bencon <bencon@udel.edu>
2014-05-11 15:58:53 -0400

    allows you to run multiple processes at once

---
+++ redirect.py
@@ -0,0 +1,29 @@
+import time
+import sys
+import subprocess
+import io
+
+if __name__ == '__main__':
+	valid = True
+	serialString = sys.argv[1]
+	interface = sys.argv[2]
+	number = sys.argv[3]
+	if number == str(1):
+		filename = 'test.log1'
+		f = open("antennaData1","w")
+	elif number == str(2):
+		filename = 'test.log2'
+		f = open("antennaData2","w")
+	else:
+		print "Invalid arguments for redirect.py"
+		valid = False
+
+	if (valid):
+		filename = 'test.log'
+		with io.open(filename, 'wb') as writer, io.open(filename, 'rb', 1) as reader:
+		    process = subprocess.Popen(["python","sweep.py",serialString,interface], stdout=writer)
+		    while process.poll() is None:
+			f.write(reader.read())
+			time.sleep(0.5)
+		    # Read the remaining
+		    f.write(reader.read())
